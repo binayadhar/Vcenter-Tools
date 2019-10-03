a7475457bec418f0fb156616c88e61f9863d9261
Dhar <BinayaKumar.Dhar@thomsonreuters.com>
2019-10-03 12:41:54 +0530

    Version 2
    Added Version 2 of the script

---
+++ README.txt
@@ -0,0 +1,39 @@
+This script is made for automation certain task in VMware ESXI Host OR Vcenters.
+The Script will perform the following functions.
+1. Rename BULK VM
+2. POWER OFF BULK VM
+3. POWER ONN BULK VM
+4. Rename back to the server names defined in the txt file.
+
+Installation:
+1. Open Powershell as ADMIN
+2. Run " Set-ExecutionPolicy RemoteSigned" and type yes..
+3. Run Install.ps1 file to install all the dependency.
+
+Running the Script.
+1. Open Srvr.txt and write the VM names one by one line needed to be performed operation on
+2. Open Creds.txt and give the Vcenter/ESXI Login Credentials.
+Note: Credentials must given as first line username and the next line password.
+3. Directly run the Script by right clicking on it.
+4. Provide the Vcenter/ESXI host IP or URL without HTTP/HTTPS
+5. On Successful connection options will be displayed to perform activity.
+
+NOTE: For Delete Activity the script will rename the VMS with the hostname-Delete option.
+and the Undo option will revert back the VMs to the original name.
+---
+Version Details.
+---
+
+:Version 2:
+* Added new function to refresh server list without exiting the script.
+* Added new function to rename the server back from -Delete to original name.
+* Code optimized.
+* Lot of Bug Fixed**
+
+
+
+:Version 1:
+* Initial Development of the script.
+* Very Basic functions.
+* Not Stable
+* Lot of errors.
---
+++ VM-Tools-Updated v2.ps1
@@ -0,0 +1,123 @@
+﻿Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false -Confirm:$false |Out-Null
+function rldall()
+{
+$Script:Srvr = Get-Content srvr.txt
+$Script:credsall=Get-Content -Path .\creds.txt
+$Script:Uname = $credsall[0]
+$Script:Pass = $credsall[1]
+}
+function srvadr(){
+$Script:addr= Read-Host -Prompt " Enter the Vcenter"
+}
+function con(){
+write-host "`n Please wait, connecting to the Vcenter..."
+Connect-VIServer -Server $addr -user $Uname -pass $Pass
+}
+function rens(){
+foreach($Srvr in $Srvr){
+$dltname=($srvr+"-Delete")
+Set-VM -VM $srvr -Name $dltname -Confirm:$false
+}
+}
+function renbck()
+{
+foreach($Srvr in $Srvr){
+$dltname=($srvr+"-Delete")
+Set-VM -VM $dltname -Name $srvr -Confirm:$false
+}
+}
+function strt(){
+foreach($Srvr in $Srvr){
+Start-VM -VM $Srvr
+}
+}
+function stp(){
+foreach($Srvr in $Srvr){
+Stop-VM -VM $Srvr -Confirm:$false
+}
+}
+function sts(){
+foreach($Srvr in $Srvr){
+Get-VM -Name $Srvr
+}
+}
+function auth(){
+   Clear-Host;
+   "This script has been written and developed by Binaya Kumar Dhar
+    Email: binaya.dhara@gmail.com
+    social:https://www.facebook.com/VinayaRocks"
+    pause
+}
+rldall
+srvadr
+con
+While ($true) {
+Write-Host "`n Below are the choices:`n"
+"1. PowerOFF VM's
+2. PowerON VM's
+3. Rename VM's
+4. Undo Rename
+5. Get the status of the VM
+6. Reload Server List
+C. Credits
+0. Exit the Program
+
+"
+$ch=Read-Host -Prompt "`n Please enter your choice"
+If ($ch –eq "0") 
+   { 
+    ""
+    Write-Host -ForegroundColor red "Sad to see you go!! Adios!!"; 
+     sleep 2
+     Break;
+    }
+If ($ch –eq "C")
+    {
+     auth;
+     break;
+    }
+If (($ch -ne "1") -and ($ch -ne "2") -and ($ch -ne "3") -and ($ch -ne "4") -and ($ch -ne "5") -and ($ch -ne "6") -and ($ch -ne "0")) {
+   ""
+   Write-Host -ForegroundColor Red "Thats beyond the range, select again"
+   sleep 2
+    }
+switch ($ch) 
+        { 
+          1 {
+             stp;
+             "Command completed!"
+             sleep 2 
+            } 
+          2 {
+              strt;
+              "Command completed!"
+              sleep 2 
+            } 
+          3 {
+              rens;
+              "Command completed!"
+              sleep 2 
+             }
+           4 {
+               renbck; 
+               "Command completed!"
+               sleep 2 
+             }
+           5 {
+               sts; 
+               "Command completed!"
+               sleep 2 
+             }
+           6 {
+               rldall;
+               "Finished reading Fresh Values!"
+               sleep 2 
+             }
+           #7 {
+           #   srvadr; 
+           #   con;
+           #    "Command completed!"
+           #    sleep 2 
+             }
+            default { "Please select a correct option."}
+          }
